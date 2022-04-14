2262f69b190f43205c1da9bef3af00d767d031a4
설아아빠 <dhrod0325@naver.com>
2022-04-14 22:00:46 +0900

    refactor : 테스트 복구

--- src/test/java/baseball/domain/score/ScoreTest.java
+++ src/test/java/baseball/domain/score/ScoreTest.java
@@ -1,50 +1,49 @@
-//package baseball.domain.score;
-//
-//import baseball.domain.Ball;
-//import baseball.domain.Score;
-//import baseball.domain.Referee;
-//import baseball.domain.RefereeImpl;
-//import org.junit.jupiter.api.Test;
-//
-//import static baseball.domain.Score.*;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//
-//public class ScoreTest {
-//    private final Referee referee = new RefereeImpl();
-//
-//    public Score swing(String pitchInput, String swingInput) {
-//        return referee.calcScore(new Ball(pitchInput), new Ball(swingInput));
-//    }
-//
-//    @Test
-//    public void 테스트_낫싱() {
-//        Score score = swing("123", "456");
-//        assertEquals(score.getScoreCount(NOTHING), 3);
-//    }
-//
-//    @Test
-//    public void 테스트_1스트라이크() {
-//        Score score = swing("123", "426");
-//        assertEquals(score.getScoreCount(STRIKE), 1);
-//    }
-//
-//    @Test
-//    public void 테스트_1볼_1스트라이크() {
-//        Score score = swing("623", "426");
-//
-//        assertEquals(score.getScoreCount(BALL), 1);
-//        assertEquals(score.getScoreCount(STRIKE), 1);
-//    }
-//
-//    @Test
-//    public void 테스트_2볼() {
-//        Score score = swing("632", "426");
-//        assertEquals(score.getScoreCount(BALL), 2);
-//    }
-//
-//    @Test
-//    public void 테스트_3스트라이크() {
-//        Score score = swing("623", "623");
-//        assertEquals(score.getScoreCount(STRIKE), 3);
-//    }
-//}
+package baseball.domain.score;
+
+import baseball.domain.Ball;
+import baseball.domain.RefereeImpl;
+import baseball.domain.Score;
+import org.junit.jupiter.api.Test;
+
+import static baseball.domain.Score.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ScoreTest {
+    public Score getScore(String pitchInput, String swingInput) {
+        Score score = new Score(3);
+        score.addAll(new RefereeImpl().calcScore(new Ball(pitchInput), new Ball(swingInput)));
+        return score;
+    }
+
+    @Test
+    public void 테스트_낫싱() {
+        Score score = getScore("123", "456");
+        assertEquals(score.getScoreCount(NOTHING), 3);
+    }
+
+    @Test
+    public void 테스트_1스트라이크() {
+        Score score = getScore("123", "426");
+        assertEquals(score.getScoreCount(STRIKE), 1);
+    }
+
+    @Test
+    public void 테스트_1볼_1스트라이크() {
+        Score score = getScore("623", "426");
+
+        assertEquals(score.getScoreCount(BALL), 1);
+        assertEquals(score.getScoreCount(STRIKE), 1);
+    }
+
+    @Test
+    public void 테스트_2볼() {
+        Score score = getScore("632", "426");
+        assertEquals(score.getScoreCount(BALL), 2);
+    }
+
+    @Test
+    public void 테스트_3스트라이크() {
+        Score score = getScore("623", "623");
+        assertEquals(score.getScoreCount(STRIKE), 3);
+    }
+}
